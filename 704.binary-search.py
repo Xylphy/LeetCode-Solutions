273c39239ad74876a8a76f91a64527635a71735b
Anato-Eini <jameskennethacabal@gmail.com>
2024-11-10 12:00:00 +0800

    Binary Search

---
+++ 704.binary-search.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=704 lang=python
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+
+        return -1
+        
+# @lc code=end
+
