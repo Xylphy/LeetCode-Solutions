012017cc4aeb443c3c8a583b320113058e6adcf1
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-18 00:09:13 +0800

    Monotonic Array

--- 896.monotonic-array.py
+++ 896.monotonic-array.py
@@ -13,7 +13,6 @@ class Solution(object):
         """
         decreasing = increasing = True
 
-
         for i in range(1, len(nums)):
             if nums[i] < nums[i-1]:
                 increasing = False
