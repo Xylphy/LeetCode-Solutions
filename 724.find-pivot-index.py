3111dba17997598b90c842aecdefbceb70963ab2
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-17 20:35:48 +0800

    Find pivot index

---
+++ 724.find-pivot-index.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=724 lang=python
+#
+# [724] Find Pivot Index
+#
+
+# @lc code=start
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total_sum = sum(nums)
+        left_sum = 0
+        
+        for i in range(len(nums)):
+            if left_sum == (total_sum - left_sum - nums[i]):
+                return i
+            left_sum += nums[i]
+        
+        return -1
+
+# @lc code=end
+
