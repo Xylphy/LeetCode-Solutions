02d7d702efa3975bbf42a091b9854ddaf1e440e0
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-18 15:39:44 +0800

    Find the Maximum Achievable Number

---
+++ 2769.find-the-maximum-achievable-number.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=2769 lang=python
+#
+# [2769] Find the Maximum Achievable Number
+#
+
+# @lc code=start
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        return num + t * 2
+        
+# @lc code=end
+
