ca1c3704b73caed16755ea8006411dea7f1fef16
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-18 22:54:41 +0800

    Special Array With X elements Greater Than or Equal X

---
+++ 1608.special-array-with-x-elements-greater-than-or-equal-x.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=1608 lang=python
+#
+# [1608] Special Array With X Elements Greater Than or Equal X
+#
+
+# @lc code=start
+class Solution(object):
+    def specialArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        n = len(nums)
+        for i in range(1, n + 1):
+            if nums[n - i] >= i and (n - i == 0 or nums[n - i - 1] < i):
+                return i
+        return -1
+
+        
+# @lc code=end
+
