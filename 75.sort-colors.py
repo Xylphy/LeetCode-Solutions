3c2420052ef83b9f1f519011913eefd932d1ec66
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-19 01:20:56 +0800

    Sort Colors

---
+++ 75.sort-colors.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=75 lang=python
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        """ Dutch National Flag Algorithm """
+
+        low, mid, high = 0, 0, len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+
+        
+
+
+        
+# @lc code=end
+
