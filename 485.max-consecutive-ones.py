09f77d50a0a646f5506bfc86f739f0141d318871
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-19 15:37:39 +0800

    Max Consecutive Ones

---
+++ 485.max-consecutive-ones.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=485 lang=python
+#
+# [485] Max Consecutive Ones
+#
+
+# @lc code=start
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        max_count = 0
+
+        for num in nums:
+            if num == 1:
+                count += 1
+                max_count = max(max_count, count)
+            else:
+                count = 0
+
+        return max_count
+
+        
+# @lc code=end
+
