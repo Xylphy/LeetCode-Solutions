6d4e55be19754a6ed39b49c9def35b47d4413f23
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-19 15:41:41 +0800

    Squares of a Sorted Array

---
+++ 977.squares-of-a-sorted-array.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=977 lang=python
+#
+# [977] Squares of a Sorted Array
+#
+
+# @lc code=start
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return sorted([num * num for num in nums])
+        
+# @lc code=end
+
