8d23c24c4349442e163db795e19b57e0cfd63e1a
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-25 17:26:47 +0800

    Reverse Linked List

---
+++ 206.reverse-linked-list.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=206 lang=python
+#
+# [206] Reverse Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        slow, fast = None, head
+        while fast:
+            temp = fast.next
+            fast.next = slow
+            slow = fast
+            fast = temp
+
+        return slow
+        
+# @lc code=end
+
