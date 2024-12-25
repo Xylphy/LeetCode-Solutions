87bfaf8b3a4405c42210d2930d7790b1d1be3437
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-25 17:18:46 +0800

    Reverse Integer

---
+++ 7.reverse-integer.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=7 lang=python
+#
+# [7] Reverse Integer
+#
+
+# @lc code=start
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        dup = abs(x)
+        dup = int(str(dup)[::-1])
+        if x < 0:
+            dup = -dup
+
+        return dup if -2**31 <= dup <= 2**31 - 1 else 0
+
+        
+
+        
+# @lc code=end
+
