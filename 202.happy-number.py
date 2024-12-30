5c9b55c57d41b3ecf24281abad4d279b9cc054cb
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-30 19:50:49 +0800

    Happy Number

---
+++ 202.happy-number.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=202 lang=python
+#
+# [202] Happy Number
+#
+
+# @lc code=start
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        def get_next(num):
+            total_sum = 0
+            while num > 0:
+                num, digit = divmod(num, 10)
+                total_sum += digit ** 2
+            return total_sum
+
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+
+        return n == 1
+
+        
+# @lc code=end
+
