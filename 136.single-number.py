6fe86afc63c67a7dcc4d3d950d0d5503a1a9e5a8
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-30 19:37:03 +0800

    Single Number

---
+++ 136.single-number.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=136 lang=python
+#
+# [136] Single Number
+#
+
+# @lc code=start
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for num in nums:
+            result ^= num
+        
+        return result
+
+        
+# @lc code=end
+
