eb9648b8368db39818f697099d04b1e48bda9ff6
Anato-Eini <jameskennethacabal@gmail.com>
2025-02-08 00:14:14 +0800

    Path Sum

---
+++ 112.path-sum.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=112 lang=python
+#
+# [112] Path Sum
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+    
+        targetSum -= root.val
+
+        if not root.left and not root.right:
+            return targetSum == 0
+
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+
+
+# @lc code=end
