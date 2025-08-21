64985a61ec45596cc08f7e25a2b8d51c5e82099a
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2025-08-21 13:57:25 +0800

    Update 4.median-of-two-sorted-arrays.py

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -16,38 +16,39 @@ class Solution(object):
 
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
-
+        
         m, n = len(nums1), len(nums2)
         total_length = m + n
-        half_length = total_length // 2
+        
+        if m == 0:
+            if n % 2 == 0:
+                return (nums2[n//2 - 1] + nums2[n//2]) / 2.0
+            else:
+                return nums2[n//2]
+        
 
         left, right = 0, m
-
         while left <= right:
             partition1 = (left + right) // 2
-            partition2 = half_length - partition1
-
+            partition2 = (total_length + 1) // 2 - partition1
+            
             nums1_left_max = float("-inf") if partition1 == 0 else nums1[partition1 - 1]
             nums1_right_min = float("inf") if partition1 == m else nums1[partition1]
-
             nums2_left_max = float("-inf") if partition2 == 0 else nums2[partition2 - 1]
             nums2_right_min = float("inf") if partition2 == n else nums2[partition2]
-
+            
             if nums1_left_max <= nums2_right_min and nums2_left_max <= nums1_right_min:
                 if total_length % 2 == 0:
-                    return (
-                        max(nums1_left_max, nums2_left_max)
-                        + min(nums1_right_min, nums2_right_min)
-                    ) / 2.0
+                    return (max(nums1_left_max, nums2_left_max) + min(nums1_right_min, nums2_right_min)) / 2.0
                 else:
                     return max(nums1_left_max, nums2_left_max)
-
+            
             elif nums1_left_max > nums2_right_min:
                 right = partition1 - 1
             else:
                 left = partition1 + 1
-
-        return -1
+        
+        raise -1
 
 
 # @lc code=end
