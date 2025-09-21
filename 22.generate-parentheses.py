9e4bdcdd3844fedf348d99f01a7c0d7850ef378b
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2025-09-21 13:22:00 +0800

    Generate Parentheses

---
+++ 22.generate-parentheses.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=22 lang=python
+#
+# [22] Generate Parentheses
+#
+
+
+# @lc code=start
+
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+
+        res = []
+
+        def backtrack(s="", left=0, right=0):
+            if len(s) == 2 * n:
+                res.append(s)
+                return
+            if left < n:
+                backtrack(s + "(", left + 1, right)
+            if right < left:
+                backtrack(s + ")", left, right + 1)
+
+        backtrack()
+
+        return res
+
+
+# @lc code=end
