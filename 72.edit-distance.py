9e85e6e21dc0e69ada649b0d5c0b07d47c67f150
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2026-01-24 23:00:00 +0800

    Edit Distance

---
+++ 72.edit-distance.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=72 lang=python
+#
+# [72] Edit Distance
+#
+
+# @lc code=start
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m + 1):
+            dp[i][0] = i
+        for j in range(n + 1):
+            dp[0][j] = j
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(
+                        dp[i - 1][j] + 1,  # Deletion
+                        dp[i][j - 1] + 1,  # Insertion
+                        dp[i - 1][j - 1] + 1,  # Substitution
+                    )
+
+        return dp[m][n]
+
+
+# @lc code=end
