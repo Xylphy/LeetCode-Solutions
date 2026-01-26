187219af972dd9d076356738d6dfdb3cb1e4c1ce
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2026-01-27 06:55:43 +0800

    Minimum Absolute Difference

---
+++ 1200.minimum-absolute-difference.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=1200 lang=python
+#
+# [1200] Minimum Absolute Difference
+#
+
+# @lc code=start
+class Solution(object):
+    def minimumAbsDifference(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[List[int]]
+        """
+        arr.sort()
+        min_diff = float("inf")
+        result = []
+
+        for i in range(1, len(arr)):
+            diff = arr[i] - arr[i - 1]
+            if diff < min_diff:
+                min_diff = diff
+                result = [[arr[i - 1], arr[i]]]
+            elif diff == min_diff:
+                result.append([arr[i - 1], arr[i]])
+
+        return result
+
+
+# @lc code=end
