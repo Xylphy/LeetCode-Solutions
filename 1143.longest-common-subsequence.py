767b3a5dc17e4da69acc40c55b2deb2131525447
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2026-01-28 23:00:00 +0800

    Longest Common Subsequence

---
+++ 1143.longest-common-subsequence.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=1143 lang=python
+#
+# [1143] Longest Common Subsequence
+#
+
+# @lc code=start
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+
+        m, n = len(text1), len(text2)
+        dp = [0] * (n + 1)
+
+        for i in range(1, m + 1):
+            prev = 0
+            for j in range(1, n + 1):
+                temp = dp[j]
+                if text1[i - 1] == text2[j - 1]:
+                    dp[j] = prev + 1
+                else:
+                    dp[j] = max(dp[j], dp[j - 1])
+                prev = temp
+
+
+        return dp[n]
+
+# @lc code=end
