99540e881d019f82321119ff0b2233edafd653e7
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2026-01-29 23:00:00 +0800

    Unique Paths II

---
+++ 63.unique-paths-ii.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=63 lang=python
+#
+# [63] Unique Paths II
+#
+
+# @lc code=start
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [0] * n
+        dp[0] = 1 if obstacleGrid[0][0] == 0 else 0
+
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j - 1]
+        return dp[-1]
+
+
+# @lc code=end
