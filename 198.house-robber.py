8cc1884f6564953bee8d1eef5a9405b0197e3ca5
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2026-03-16 12:00:00 +0800

    House Robber

---
+++ 198.house-robber.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=198 lang=python
+#
+# [198] House Robber
+#
+
+# @lc code=start
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if not nums:
+            return 0
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        return dp[-1]
+        
+# @lc code=end
+
