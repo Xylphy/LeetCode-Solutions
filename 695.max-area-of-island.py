a24a725ade38e4cf3bd7b4a80357ff47b5d0653f
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2026-03-17 13:22:00 +0800

    Max Area of Island

---
+++ 695.max-area-of-island.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=695 lang=python
+#
+# [695] Max Area of Island
+#
+
+# @lc code=start
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
+
+        m, n = len(grid), len(grid[0])
+        max_area = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    max_area = max(max_area, dfs(i, j))
+
+        return max_area
+        
+# @lc code=end
+
